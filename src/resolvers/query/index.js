2b6ea13800f575b65506078a7d1ccdda9da64e12
schnie <greg@astronomer.io>
2018-12-31 12:03:04 -0500

    Add deploymentConfig query

--- src/resolvers/query/index.js
+++ src/resolvers/query/index.js
@@ -1,9 +1,11 @@
 import authConfig from "./auth-config";
 import self from "./self";
 import workspaces from "./workspaces";
+import deploymentConfig from "./deployment-config";
 
 export default {
   authConfig,
   self,
-  workspaces
+  workspaces,
+  deploymentConfig
 };
