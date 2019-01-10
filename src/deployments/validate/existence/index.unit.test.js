87e429b3d029c92672dc44af9f1fb9fcfb07aec3
schnie <greg@astronomer.io>
2019-01-09 19:23:37 -0500

    Add more tests to existence validation

--- src/deployments/validate/existence/index.unit.test.js
+++ src/deployments/validate/existence/index.unit.test.js
@@ -4,13 +4,13 @@ import * as exports from "generated/client";
 import casual from "casual";
 
 describe("validateExistence", () => {
-  test("throws correct error if exists", async () => {
+  test("throws error if duplicate named deployment exists in workspace", async () => {
     const workspaceId = casual.uuid;
     const label = casual.word;
 
     const deployments = function() {
       return {
-        id: () => casual.uuid
+        id: () => [{ id: casual.uuid }]
       };
     };
 
@@ -21,5 +21,45 @@ describe("validateExistence", () => {
     await expect(validateExistence(workspaceId, label)).rejects.toThrow(
       new DuplicateDeploymentLabelError(label)
     );
+  });
+
+  test("does not throw error if updating current deployment", async () => {
+    const workspaceId = casual.uuid;
+    const label = casual.word;
+    const deploymentId = casual.uuid;
+
+    const deployments = function() {
+      return {
+        id: () => [{ id: deploymentId }]
+      };
+    };
+
+    // Set up our spy.
+    jest.spyOn(exports.prisma, "deployments").mockImplementation(deployments);
+
+    // Call the function.
+    await expect(
+      validateExistence(workspaceId, label, deploymentId)
+    ).resolves.not.toThrow();
+  });
+
+  test("throws error if different deployment in workspace has label", async () => {
+    const workspaceId = casual.uuid;
+    const label = casual.word;
+    const deploymentId = casual.uuid;
+
+    const deployments = function() {
+      return {
+        id: () => [{ id: casual.uuid }]
+      };
+    };
+
+    // Set up our spy.
+    jest.spyOn(exports.prisma, "deployments").mockImplementation(deployments);
+
+    // Call the function.
+    await expect(
+      validateExistence(workspaceId, label, deploymentId)
+    ).rejects.toThrow(new DuplicateDeploymentLabelError(label));
   });
 });
