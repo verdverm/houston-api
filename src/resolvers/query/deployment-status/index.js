b9fecc19c4cc27e2e46bc4406dc1da9f20f867f1
schnie <greg@astronomer.io>
2019-04-01 09:13:35 -0400

    Add missing file

---
+++ src/resolvers/query/deployment-status/index.js
@@ -0,0 +1,10 @@
+/*
+ * Get status of deployment pods.
+ * @param {Object} parent The result of the parent resolver.
+ * @param {Object} args The graphql arguments.
+ * @param {Object} ctx The graphql context.
+ * @return {DeploymentConfig} The deployment config.
+ */
+export default async function deploymentStatus() {
+  return {};
+}
