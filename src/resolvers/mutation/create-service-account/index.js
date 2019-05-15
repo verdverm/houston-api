704f30df002ee0fc6ec5446442c42323975f7fd6
schnie <greg@astronomer.io>
2019-05-15 11:31:40 -0400

    Add checkPermission to create-service-account

--- src/resolvers/mutation/create-service-account/index.js
+++ src/resolvers/mutation/create-service-account/index.js
@@ -1,5 +1,5 @@
 import fragment from "./fragment";
-import { serviceAccountRoleMappings } from "rbac";
+import { checkPermission, serviceAccountRoleMappings } from "rbac";
 import { PermissionError } from "errors";
 import { compact, includes } from "lodash";
 import { addFragmentToInfo } from "graphql-binding";
@@ -19,17 +19,12 @@ export default async function createServiceAccount(parent, args, ctx, info) {
   const { label, category, entityType: upperEntityType, entityUuid } = args;
   const entityType = upperEntityType.toLowerCase();
 
-  // Get a list of ids of entityType that this user has access to.
-  const ids = compact(
-    ctx.user.roleBindings.map(binding =>
-      binding[entityType] ? binding[entityType].id : null
-    )
+  checkPermission(
+    ctx.user,
+    `${entityType}.deployments.list`,
+    entityType,
+    entityUuid
   );
-
-  // Throw error if the incoming entityId is not in the list of ids for this user.
-  if (entityUuid && !includes(ids, entityUuid)) {
-    throw new PermissionError();
-  }
 
   // Create the base mutation.
   const mutation = {
