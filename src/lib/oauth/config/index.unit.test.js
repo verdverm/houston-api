3f42b7f08969f1e9b0063560b79ccee10dfd0bac
Ash Berlin-Taylor <ash_github@firemirror.com>
2019-07-12 18:52:57 +0100

    Make Houston return a list of configured Auth providers (#86)
    This change (along with the matching change in Orbit) allows us to
    define extra auth providers in the Houston config and have them show up
    in the front end.
    
    (cherry picked from commit 7b05935c617e4222a378dc07ddb704a9d693d0d7)

--- src/lib/oauth/config/index.unit.test.js
+++ src/lib/oauth/config/index.unit.test.js
@@ -35,6 +35,7 @@ describe("oauth configuration", () => {
     test("should use the Auth0 provider", async () => {
       const client = await getClient("google");
       expect(client.issuer.metadata.name).toBe("auth0");
+      expect(client.metadata.displayName).toBe("Google");
     });
 
     test("should include connection in URL", async () => {
@@ -42,6 +43,45 @@ describe("oauth configuration", () => {
       const params = new URLSearchParams(client.authUrl({}));
 
       expect(params.get("connection")).toBe("google-oauth2");
+    });
+
+    test("should include right integration in state", async () => {
+      const client = await getClient("google");
+      const params = new URLSearchParams(client.authUrl({}));
+
+      const state = JSON.parse(params.get("state"));
+      expect(state.integration).toBe("google-oauth2");
+    });
+  });
+
+  describe("When github is enabled", () => {
+    beforeAll(() => {
+      providerCfg.github.enabled = true;
+    });
+
+    afterAll(() => {
+      ClientCache.delete("github");
+    });
+
+    test("should use the Auth0 provider", async () => {
+      const client = await getClient("github");
+      expect(client.issuer.metadata.name).toBe("auth0");
+      expect(client.metadata.displayName).toBe("GitHub");
+    });
+
+    test("should include connection in URL", async () => {
+      const client = await getClient("github");
+      const params = new URLSearchParams(client.authUrl({}));
+
+      expect(params.get("connection")).toBe("github");
+    });
+
+    test("should include right integration in state", async () => {
+      const client = await getClient("github");
+      const params = new URLSearchParams(client.authUrl({}));
+
+      const state = JSON.parse(params.get("state"));
+      expect(state.integration).toBe("github");
     });
   });
 
@@ -57,6 +97,7 @@ describe("oauth configuration", () => {
     test("should use Google directly", async () => {
       const client = await getClient("google");
       expect(client.issuer.metadata.name).toBe("google");
+      expect(client.metadata.displayName).toBe("Google");
     });
 
     test("should not include connection in URL", async () => {
