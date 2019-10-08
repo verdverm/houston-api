7c0aafb9919bd68830a3e19df0b6437f22864d8e
sjmiller609 <sjmiller609@gmail.com>
2019-10-08 13:54:24 +0000

    Add /v1/metrics route (#134)
    * Add /metrics endpoint
    
    * Prometheus metricsPath at '/v1/metrics'
    
    * Move prometheus bundle to v1 module

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -5,6 +5,7 @@ import healthz from "./healthz";
 import oauth from "./oauth";
 import registry from "./registry";
 import express from "express";
+import promBundle from "express-prom-bundle";
 
 // Create top level router for v1.
 const router = new express.Router();
@@ -16,6 +17,14 @@ router.use("/elasticsearch", elasticsearch);
 router.use("/healthz", healthz);
 router.use("/oauth", oauth);
 router.use("/registry", registry);
+
+// Attach prometheus route.
+router.use(
+  promBundle({
+    includeMethod: true,
+    metricsPath: "/v1/metrics"
+  })
+);
 
 // Export the v1 router.
 export default router;
