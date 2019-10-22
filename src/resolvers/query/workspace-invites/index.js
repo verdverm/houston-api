6a22cd17aee70ddd2328a8bcf0dfd43e15b639f9
Samantha Black (Ingram) <supersamblack@gmail.com>
2019-10-22 15:14:25 -0400

    Rework workspaceUsers and workspaceInvites (#149)

--- src/resolvers/query/workspace-invites/index.js
+++ src/resolvers/query/workspace-invites/index.js
@@ -1,15 +1,35 @@
 /*
- * Get a list of invites for a workpace.
+ * Get a list of invites for a workspace,
  * @param {Object} parent The result of the parent resolver.
  * @param {Object} args The graphql arguments.
  * @param {Object} ctx The graphql context.
  * @return {[]Invites} A list of invites.
  */
-export default async function invites(parent, args, ctx) {
-  let { workspaceUuid, email } = args;
-  email = email.toLowerCase();
+export default async function workspaceInvites(parent, args, ctx) {
+  // Build the users query.
+  const query = invitesQuery(args);
 
-  return ctx.db.query.inviteTokens({
-    where: { workspace: { id: workspaceUuid }, email }
+  // Run final query
+  return await ctx.db.query.inviteTokens({
+    where: {
+      workspace: { id: args.workspaceUuid },
+      ...query
+    }
   });
+}
+
+/*
+ * Build the invites query based on args.
+ * @param {Object} args The graphql arguments.
+ * @param {Object} ctx The graphql context.
+ * @return {Object} The users query.
+ */
+export function invitesQuery(args) {
+  // Pull out some args.
+  const { email } = args;
+
+  // If we have an email use it.
+  if (email) return { email_contains: email.toLowerCase() };
+
+  return null;
 }
--- src/resolvers/query/workspace-users/index.js
+++ src/resolvers/query/workspace-users/index.js
@@ -8,14 +8,34 @@ import { addFragmentToInfo } from "graphql-binding";
  * @return users on this specific workspace
  */
 export default async function workspaceUsers(parent, args, ctx, info) {
+  // Build the users query.
+  const query = invitesQuery(args);
+
   return ctx.db.query.users(
     {
       where: {
         roleBindings_some: {
           workspace: { id: args.workspaceUuid }
-        }
+        },
+        ...query
       }
     },
     addFragmentToInfo(info, userFragment)
   );
+}
+
+/*
+ * Build the workspaceUsers query based on args.
+ * @param {Object} args The graphql arguments.
+ * @param {Object} ctx The graphql context.
+ * @return {Object} The users query.
+ */
+export function invitesQuery(args) {
+  // Pull out some args.
+  const { email } = args;
+
+  // If we have an email use it.
+  if (email) return { username_contains: email.toLowerCase() };
+
+  return null;
 }
