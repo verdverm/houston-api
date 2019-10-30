4c79da12a0b377ae679d0b57ef82b7a6cd7d7c8e
Ash Berlin-Taylor <ash_github@firemirror.com>
2019-10-30 19:55:21 +0000

    Record Labels and Env from pushed docker images (#155)
    This will le us know what version of Airflow that a docker image
    claims to be. (Claim, as this could be faked by end-user docker image if
    they so desired).
    
    Right now we don't do anything with this information

--- src/routes/v1/registry/events/index.unit.test.js
+++ src/routes/v1/registry/events/index.unit.test.js
@@ -1,8 +1,10 @@
 import router from "./index";
+import * as postExports from "./post";
 import { prisma } from "generated/client";
 import casual from "casual";
 import request from "supertest";
 import express from "express";
+import nock from "nock";
 import { DOCKER_REGISTRY_CONTENT_TYPE } from "constants";
 
 jest.mock("generated/client", () => {
@@ -34,6 +36,20 @@ describe("POST /registry-events", () => {
       .mockName("updateDeployment")
       .mockReturnValue({});
 
+    const labels = Symbol(),
+      env = Symbol(),
+      digest =
+        "sha256:907b4a633d31872f7fc9b4cb22998b9de4c25f8cc8f08529ca56c2ace698e541";
+
+    const extractImageMetadata = jest
+      .spyOn(postExports, "extractImageMetadata")
+      .mockImplementation(() => ({ labels, env }));
+
+    prisma.upsertDockerImage = jest
+      .fn()
+      .mockName("upsertDockerImage")
+      .mockReturnValue({});
+
     const res = await request(app)
       .post("/")
       .set("Content-Type", DOCKER_REGISTRY_CONTENT_TYPE)
@@ -44,7 +60,8 @@ describe("POST /registry-events", () => {
             action: "push",
             target: {
               repository: "cosmic-dust-1234/airflow",
-              tag: "cli-1"
+              tag: "cli-1",
+              digest: digest
             },
             request: {
               host: casual.domain
@@ -53,8 +70,24 @@ describe("POST /registry-events", () => {
         ]
       });
 
+    const name = "cosmic-dust-1234/airflow:cli-1";
+
     expect(prisma.deployment).toHaveBeenCalledTimes(1);
     expect(prisma.updateDeployment).toHaveBeenCalledTimes(1);
+    expect(extractImageMetadata).toHaveBeenCalledTimes(1);
+    expect(prisma.upsertDockerImage).toHaveBeenCalledTimes(1);
+    expect(prisma.upsertDockerImage).toHaveBeenCalledWith({
+      where: { name },
+      update: { labels, env, digest },
+      create: {
+        name,
+        labels,
+        env,
+        digest,
+        deployment: { connect: { releaseName: "cosmic-dust-1234" } },
+        tag: "cli-1"
+      }
+    });
     expect(res.statusCode).toBe(200);
   });
 
@@ -105,5 +138,160 @@ describe("POST /registry-events", () => {
     expect(prisma.deployment).toHaveBeenCalledTimes(0);
     expect(prisma.updateDeployment).toHaveBeenCalledTimes(0);
     expect(res.statusCode).toBe(200);
+  });
+});
+
+describe("extractImageMetadata", () => {
+  afterEach(() => {
+    nock.cleanAll();
+  });
+
+  const event = {
+    action: "push",
+    actor: {},
+    id: "4171805b-43f8-4f98-acbc-923db2a6c183",
+    request: {
+      addr: "172.17.0.1:44554",
+      host: "localhost:5000",
+      id: "389735b8-842c-407e-9ac9-ff51d904505c",
+      method: "PUT",
+      useragent:
+        "docker/19.03.3 go/go1.12.10 git-commit/a872fc2 kernel/4.14.131-linuxkit os/linux arch/amd64 UpstreamClient(Docker-Client/18.09.1 (darwin))"
+    },
+    source: {
+      addr: "509d84c15f87:5001",
+      instanceID: "5a935f4c-180a-4d14-b0fc-7e57c475778d"
+    },
+    target: {
+      digest:
+        "sha256:907b4a633d31872f7fc9b4cb22998b9de4c25f8cc8f08529ca56c2ace698e541",
+      length: 2402,
+      mediaType: "application/vnd.docker.distribution.manifest.v2+json",
+      repository: "telescopic-sun-2787/airflow",
+      size: 2402,
+      tag: "cli-1",
+      url:
+        "http://localhost:5000/v2/telescopic-sun-2787/airflow/manifests/sha256:907b4a633d31872f7fc9b4cb22998b9de4c25f8cc8f08529ca56c2ace698e541"
+    },
+    timestamp: "2019-10-27T15:28:05.9491821Z"
+  };
+
+  const manifestResponse = {
+    config: {
+      digest:
+        "sha256:eb7dc89de609bc3b6e5af651bfb6c7879e33c6b3a9ab09cc0a0ec0c7d050a5b6",
+      mediaType: "application/vnd.docker.container.image.v1+json",
+      size: 10621
+    },
+    layers: [
+      /*...*/
+    ]
+  };
+
+  const configBlobResponse = {
+    architecture: "amd64",
+    config: {
+      Hostname: "",
+      Domainname: "",
+      User: "",
+      AttachStdin: false,
+      AttachStdout: false,
+      AttachStderr: false,
+      ExposedPorts: { "5555/tcp": {}, "8080/tcp": {}, "8793/tcp": {} },
+      Tty: false,
+      OpenStdin: false,
+      StdinOnce: false,
+      Env: [
+        "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+        "AIRFLOW_REPOSITORY=https://github.com/astronomer/airflow",
+        "AIRFLOW_MODULE=git+https://github.com/astronomer/airflow@1.10.5-1#egg=apache-airflow[all, statsd, elasticsearch]",
+        "AIRFLOW_HOME=/usr/local/airflow",
+        "PYMSSQL_BUILD_WITH_BUNDLED_FREETDS=1",
+        "PYTHONPATH=/usr/local/airflow",
+        "AIRFLOW_GPL_UNIDECODE=True",
+        "ASTRONOMER_USER=astro",
+        "ASTRONOMER_GROUP=astro",
+        "GUNICORN_CMD_ARGS=--preload"
+      ],
+      Cmd: null,
+      ArgsEscaped: true,
+      Image:
+        "sha256:bc3917a3d61fc9cf2af44ddb6a07556e6ea4ebae4ef6331663e569df14491d17",
+      Volumes: null,
+      WorkingDir: "/usr/local/airflow",
+      Entrypoint: ["tini", "--", "/entrypoint"],
+      OnBuild: null,
+      Labels: {
+        "io.astronomer.docker": "true",
+        "io.astronomer.docker.airflow.version": "1.10.5",
+        "io.astronomer.docker.build.number": "1403",
+        "io.astronomer.docker.component": "airflow",
+        "io.astronomer.docker.module": "airflow",
+        maintainer: "Astronomer \u003chumans@astronomer.io\u003e"
+      }
+    },
+    container_config: {
+      /*...*/
+    },
+    history: [
+      /*...*/
+    ],
+    os: "linux",
+    rootfs: {
+      /* ... */
+    }
+  };
+
+  test("ignores unknown manifest types", async () => {
+    // Submit a clone of the event with different media type
+    const ev = JSON.parse(JSON.stringify(event));
+    ev.target.mediaType =
+      "application/vnd.docker.distribution.manifest.v1+json";
+
+    const scope = nock("http://localhost:5000/")
+      .get(/.*/)
+      .replyWithError("should not be called");
+
+    await expect(postExports.extractImageMetadata(ev)).resolves.toBeUndefined();
+
+    // Should not have made any http requests
+    expect(scope.isDone()).toBe(false);
+  });
+
+  test("fetches config blob", async () => {
+    const scope = nock("http://localhost:5000/")
+      .get(`/v2/telescopic-sun-2787/airflow/manifests/${event.target.digest}`)
+      .reply(200, manifestResponse)
+      .get(
+        `/v2/telescopic-sun-2787/airflow/blobs/${
+          manifestResponse.config.digest
+        }`
+      )
+      .reply(200, configBlobResponse);
+
+    await expect(postExports.extractImageMetadata(event)).resolves.toEqual({
+      env: [
+        "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin",
+        "AIRFLOW_REPOSITORY=https://github.com/astronomer/airflow",
+        "AIRFLOW_MODULE=git+https://github.com/astronomer/airflow@1.10.5-1#egg=apache-airflow[all, statsd, elasticsearch]",
+        "AIRFLOW_HOME=/usr/local/airflow",
+        "PYMSSQL_BUILD_WITH_BUNDLED_FREETDS=1",
+        "PYTHONPATH=/usr/local/airflow",
+        "AIRFLOW_GPL_UNIDECODE=True",
+        "ASTRONOMER_USER=astro",
+        "ASTRONOMER_GROUP=astro",
+        "GUNICORN_CMD_ARGS=--preload"
+      ],
+      labels: {
+        "io.astronomer.docker": "true",
+        "io.astronomer.docker.airflow.version": "1.10.5",
+        "io.astronomer.docker.build.number": "1403",
+        "io.astronomer.docker.component": "airflow",
+        "io.astronomer.docker.module": "airflow",
+        maintainer: "Astronomer <humans@astronomer.io>"
+      }
+    });
+
+    expect(scope.isDone()).toBeTruthy();
   });
 });
--- src/routes/v1/registry/events/post.js
+++ src/routes/v1/registry/events/post.js
@@ -1,9 +1,12 @@
 import { generateHelmValues } from "deployments/config";
 import { prisma } from "generated/client";
+import { createDockerJWT } from "registry/jwt";
 import isValidTaggedDeployment from "deployments/validate/docker-tag";
 import log from "logger";
 import commander from "commander";
+import { version } from "utilities";
 import { merge } from "lodash";
+import got from "got";
 import { DEPLOYMENT_AIRFLOW } from "constants";
 
 /*
@@ -30,6 +33,39 @@ export default async function(req, res) {
       // Get the existing config for this deployment.
       const config = await prisma.deployment({ releaseName }).config();
 
+      try {
+        const imageMetadata = await exports.extractImageMetadata(ev);
+        await prisma.upsertDockerImage({
+          where: {
+            name: `${ev.target.repository}:${tag}`
+          },
+          update: {
+            labels: imageMetadata.labels,
+            env: imageMetadata.env,
+            digest: ev.target.digest
+          },
+          create: {
+            name: `${ev.target.repository}:${tag}`,
+            deployment: {
+              connect: { releaseName }
+            },
+            labels: imageMetadata.labels,
+            env: imageMetadata.env,
+            tag: tag,
+            digest: ev.target.digest
+          }
+        });
+      } catch (e) {
+        if (e instanceof got.GotError) {
+          log.error(`Error fetching ${e.url} ${e}`);
+        } else {
+          log.error(`Error upserting image metadata: ${e}`);
+        }
+        // Don't cause an error here to fail the request, that causes the
+        // Registry to keep trying to send the webhook to us!
+        return;
+      }
+
       // Merge the new image tag in.
       const updatedConfig = merge({}, config, {
         images: { airflow: { repository, tag } }
@@ -54,4 +90,53 @@ export default async function(req, res) {
   );
 
   res.sendStatus(200);
+}
+
+const MEDIATYPE_DOCKER_MANIFEST_V2 =
+  "application/vnd.docker.distribution.manifest.v2+json";
+
+export async function extractImageMetadata(ev) {
+  if (ev.target.mediaType != MEDIATYPE_DOCKER_MANIFEST_V2) {
+    log.debug(`Ignoring non-manifest type ${ev.target.mediaType}`);
+    return;
+  }
+
+  const expiration = 30;
+  const sub = "system";
+  const claims = [
+    {
+      type: "repository",
+      actions: ["pull"],
+      name: ev.target.repository
+    }
+  ];
+  const dockerJWT = await createDockerJWT(sub, claims, expiration);
+
+  const client = got.extend({
+    headers: {
+      Accept: MEDIATYPE_DOCKER_MANIFEST_V2,
+      Authorization: `Bearer ${dockerJWT}`,
+      "User-Agent": `houston/${version()}`
+    },
+    json: true
+  });
+
+  log.info(`Fectching manifest from ${ev.target.url}`);
+  const manifest = (await client.get(ev.target.url)).body;
+
+  if (!manifest.config) {
+    log.debug("No config field in manifest!");
+    return;
+  }
+
+  const configURL = ev.target.url.replace(
+    /manifests\/.*$/,
+    `blobs/${manifest.config.digest}`
+  );
+  log.debug(`Fectching image config from ${configURL}`);
+  const imageMetadata = (await client.get(configURL, {
+    headers: { Accept: manifest.config.mediaType }
+  })).body;
+
+  return { labels: imageMetadata.config.Labels, env: imageMetadata.config.Env };
 }
