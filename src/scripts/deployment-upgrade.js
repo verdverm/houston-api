b9d825ff57d21f886604c04f917127870a6cbeb5
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2019-11-29 17:00:57 +0200

    tweak logic of automatic upgrade deployment

--- src/scripts/deployment-upgrade.js
+++ src/scripts/deployment-upgrade.js
@@ -5,23 +5,39 @@
 import "dotenv/config";
 import log from "logger";
 import { prisma } from "generated/client";
+import commander from "commander";
+import { generateHelmValues } from "deployments/config";
 import yargs from "yargs";
+import { DEPLOYMENT_AIRFLOW } from "constants";
 
 /*
  * Upgrade deployments
  */
 async function deploymentUpgrade() {
-  log.info("Starting registry cleanup");
+  log.info("Starting automatic deployment upgrade");
   const deployments = await prisma.deployments({}, `{ releaseName, version }`);
   if (deployments.length === 0) {
     log.info("There is no deployments to delete :(");
     return;
   }
+
   for (const deployment of deployments) {
     if (deployment.version !== argv["airflowChartVersion"]) {
-      await prisma.upgradeDeployment({
-        deploymentUuid: deployment.deploymentUuid,
-        version: argv["airflowChartVersion"]
+      const updatedDeployment = await prisma.mutation.updateDeployment(
+        {
+          where: { id: deployment.deploymentUuid },
+          data: { version: argv["airflowChartVersion"] }
+        },
+        `{ releaseNam, version }`
+      );
+
+      await commander.request("updateDeployment", {
+        releaseName: updatedDeployment.releaseName,
+        chart: {
+          name: DEPLOYMENT_AIRFLOW,
+          version: updatedDeployment.version
+        },
+        rawConfig: JSON.stringify(generateHelmValues(updatedDeployment))
       });
     }
   }
