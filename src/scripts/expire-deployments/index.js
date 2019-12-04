89c2a2289713f8d703caf0e11a3147bfcb4a0efe
Andrii Soldatenko <andrii.soldatenko@gmail.com>
2019-12-04 16:43:34 +0200

    Clean up expired trial deployments (#184)
    * init scripts/expire-deployments.js
    
    * respect master folder structure
    
    * implement soft deletion of expired deployments
    
    * fix code
    
    * Update src/scripts/expire-deployments/index.js
    
    * Update src/schema.graphql

---
+++ src/scripts/expire-deployments/index.js
@@ -0,0 +1,48 @@
+/*
+ * Script to soft delete expired deployments
+ */
+import "dotenv/config";
+import log from "logger";
+import { prisma } from "generated/client";
+
+/*
+ * Soft deletion of expired deployments
+ */
+async function index() {
+  log.info("Starting soft deletion of expired deployments");
+
+  const expireDate = new Date();
+
+  // Find all suspended deployments.
+  const deployments = await prisma
+    .deployments({ where: { workspace: { trialEndsAt_lte: expireDate } } })
+    .$fragment(`{ id releaseName workspace { id trialEndsAt } }`);
+
+  // Return early if we have no deployments.
+  if (deployments.length === 0) {
+    log.info("There are no deployments to delete :(");
+    return;
+  }
+
+  for (const deployment of deployments) {
+    // Pull out some deployment fields.
+    const { releaseName, id } = deployment;
+
+    log.info(`Updating expired deployment ${releaseName}`);
+
+    // Update the database.
+    await prisma.updateDeployment({
+      where: { id: id },
+      data: { deletedAt: new Date() }
+    });
+  }
+
+  log.info("Soft deletion of expired deployments has been finished!");
+}
+
+// When a file is run directly from Node, require.main is set to its module.
+if (require.main === module) {
+  index().catch(err => {
+    log.error(err);
+  });
+}
